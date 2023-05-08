609270e1e6608727202f49be130fb05697dfe7e7
Jakob Blume <jakob.blume@mailbox.org>
2023-05-08 13:08:39 +0200

    initial commit

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package sample_go_with_vulnerable_dep
 
-import "github.com/hashicorp/vault/shamir"
+import (
+	"fmt"
+	"github.com/hashicorp/vault/shamir"
+)
 
 func Hello() {
 	_, err := shamir.Combine([][]byte{})
 	if err != nil {
 		return
 	}
+}
+
+func Hello2() {
+	fmt.Println("Hello World")
 }
